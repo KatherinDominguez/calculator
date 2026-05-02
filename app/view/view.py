d56c62d96c7cd16b2d832fb9fea7e2bd9730b503
Ian Hans Escobar Urey <iangameplayhans@gmail.com>
2026-05-01 22:50:59 -0400

    feat:add operators buttons in view.py

--- app/view/view.py
+++ app/view/view.py
@@ -15,8 +15,23 @@ def create_buttons_number():
     btn = tk.Button(root,text=0)
     btn.grid(row=4,column=0)
 
+def create_buttons_operators():
+    btn_sum = tk.Button(root,text="+")
+    btn_substract = tk.Button(root,text="-")
+    btn_times = tk.Button(root,text="*")
+    btn_divide = tk.Button(root,text="/")
+    btn_equal = tk.Button(root,text="=")
+
+    btn_sum.grid(row=1,column=3)
+    btn_substract.grid(row=2,column=3)
+    btn_times.grid(row=3,column=3)
+    btn_divide.grid(row=4,column=3)
+    btn_equal.grid(row=3,column=4,rowspan=2)
+
+
 create_screen()
 create_buttons_number()
+create_buttons_operators()
 
 root.mainloop()
 
