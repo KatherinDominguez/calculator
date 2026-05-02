61b1902ce287cec15d978559896ac999e3f70c6e
Maribel Choque Medrano <maribelchoquemedrano02@gmail.com>
2026-05-02 17:12:32 -0400

    test: add unit test for the multiplication function

--- app/tests/test_calculator.py
+++ app/tests/test_calculator.py
@@ -10,3 +10,8 @@ class TestCalculadora(unittest.TestCase):
         self.assertEqual(resta(10, 3), 7)
         self.assertEqual(resta(0, 0), 0)
         self.assertEqual(resta(5, 10), -5)
+    
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(3, 4), 12)
+        self.assertEqual(multiplicacion(0, 99), 0)
+        self.assertEqual(multiplicacion(-2, 5), -10)
