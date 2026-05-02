565405ae0617fa5907318be78191318c2b72dac1
Maribel Choque Medrano <maribelchoquemedrano02@gmail.com>
2026-05-02 17:10:29 -0400

    test: add unit tests for the subtraction function

--- app/tests/test_calculator.py
+++ app/tests/test_calculator.py
@@ -5,3 +5,8 @@ class TestCalculadora(unittest.TestCase):
         self.assertEqual(suma(2, 3), 5)
         self.assertEqual(suma(0, 0), 0)
         self.assertEqual(suma(100, 200), 300)
+
+    def test_resta(self):
+        self.assertEqual(resta(10, 3), 7)
+        self.assertEqual(resta(0, 0), 0)
+        self.assertEqual(resta(5, 10), -5)
