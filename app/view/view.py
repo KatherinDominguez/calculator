790838ba54e96444c9a1fe8b76aba093e3976f40
Ian Hans Escobar Urey <iangameplayhans@gmail.com>
2026-05-01 22:32:31 -0400

    feat:add screen label in view.py

--- app/view/view.py
+++ app/view/view.py
@@ -2,6 +2,12 @@ import tkinter as tk
 
 root = tk.Tk()
 
+value_screen = tk.StringVar(value="")
+
+def create_screen():
+    label = tk.Label(root,textvariable=value_screen)
+    label.grid(row=0,column=0,columnspan=3)
+
 def create_buttons_number():
     for i in range(9):
         btn = tk.Button(root,text=i+1)
@@ -9,6 +15,7 @@ def create_buttons_number():
     btn = tk.Button(root,text=0)
     btn.grid(row=4,column=0)
 
+create_screen()
 create_buttons_number()
 
 root.mainloop()
