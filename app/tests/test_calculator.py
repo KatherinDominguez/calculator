b1a0d3f48105077cc9d114a0acf1f506c903909f
Maribel Choque Medrano <maribelchoquemedrano02@gmail.com>
2026-05-02 17:08:12 -0400

    test: add unit tests for the addition function

--- app/tests/test_calculator.py
+++ app/tests/test_calculator.py
@@ -1,4 +1,7 @@
 import unittest
 from model import suma, resta, multiplicacion, division
 class TestCalculadora(unittest.TestCase):
-    
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+        self.assertEqual(suma(0, 0), 0)
+        self.assertEqual(suma(100, 200), 300)
