d8d228aaa9ce919c3cd42ab3b5779a1e48feb342
Ian Hans Escobar Urey <iangameplayhans@gmail.com>
2026-05-01 23:33:28 -0400

    feat:add function update_screen_value in view.py

--- app/view/view.py
+++ app/view/view.py
@@ -1,8 +1,18 @@
+from functools import partial
 import tkinter as tk
 
 root = tk.Tk()
 
 value_screen = tk.StringVar(value="")
+
+def update_screen_value(value):
+    if value_screen.get() == "0" and value == 0:
+        return
+    elif value_screen.get()== "0":
+        value_screen.set("")
+    value_screen.set(value_screen.get()+ str(value))
+
+
 
 def create_screen():
     label = tk.Label(root,textvariable=value_screen)
@@ -10,9 +20,9 @@ def create_screen():
 
 def create_buttons_number():
     for i in range(9):
-        btn = tk.Button(root,text=i+1)
+        btn = tk.Button(root,text=i+1,command=partial(update_screen_value,i+1))
         btn.grid(row=i//3+1,column=i%3)
-    btn = tk.Button(root,text=0)
+    btn = tk.Button(root,text=0,command=partial(update_screen_value,0))
     btn.grid(row=4,column=0)
 
 def create_buttons_operators():
